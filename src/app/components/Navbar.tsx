9dc2cd42671ac02018c5196d270361ff6cd476f7
kadelcode <kadelcode@gmail.com>
2025-04-15 23:37:33 +0100

    Add link to logo

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -5,11 +5,11 @@ export default function Navbar() {
     return (
         <nav className="w-full py-4 px-6 shadow-md bg-white sticky top-0 z-50">
             <div className="max-w-7xl mx-auto flex items-center justify-between">
-                <div className="text-xl font-bold flex items-center text-gray-900"
+                <Link href="/" className="text-xl font-bold flex items-center text-gray-900"
                 >
                     <PiYoutubeLogo size={30} className="text-rose-600 inline" />
                     <span>ReelGrab</span>
-                </div>
+                </Link>
                 <ul className="flex gap-6 text-sm text-gray-700 font-medium">
                     <li><a href="#features">Features</a></li>
                     <li><a href="#how-it-works">How it Works</a></li>
