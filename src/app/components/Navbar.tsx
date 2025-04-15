9db02b3706d1aba091da3aa81da994f36c514661
kadelcode <kadelcode@gmail.com>
2025-04-15 23:31:34 +0100

    Change brand name

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,8 +1,15 @@
+import { PiYoutubeLogo } from "react-icons/pi";
+import Link from "next/link";
+
 export default function Navbar() {
     return (
         <nav className="w-full py-4 px-6 shadow-md bg-white sticky top-0 z-50">
             <div className="max-w-7xl mx-auto flex items-center justify-between">
-                <h1 className="text-xl font-bold text-blue-600">YTDownloader</h1>
+                <div className="text-xl font-bold flex items-center text-gray-900"
+                >
+                    <PiYoutubeLogo size={30} className="text-rose-600 inline" />
+                    <span>ReelGrab</span>
+                </div>
                 <ul className="flex gap-6 text-sm text-gray-700 font-medium">
                     <li><a href="#features">Features</a></li>
                     <li><a href="#how-it-works">How it Works</a></li>
