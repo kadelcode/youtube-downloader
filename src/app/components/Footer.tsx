d86e762149ef9abcb2360ba07105c13e87653a2c
kadelcode <kadelcode@gmail.com>
2025-04-16 08:37:01 +0100

    Add footer section

---
+++ src/app/components/Footer.tsx
@@ -0,0 +1,8 @@
+export default function Footer() {
+    return (
+      <footer className="text-center py-6 text-sm bg-gray-300 text-gray-800">
+        <p>&copy; {new Date().getFullYear()} YTDownloader. Built with ❤️ using Next.js + TailwindCSS.</p>
+      </footer>
+    );
+}
+  
